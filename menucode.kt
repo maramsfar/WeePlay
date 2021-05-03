f9ea3a567a37d13a72491abc79cefcb521e5b417
CyrineM <82709871+CyrineM@users.noreply.github.com>
2021-05-04 00:29:31 +0100

    Update menucode.kt

--- menucode.kt
+++ menucode.kt
@@ -1,1 +1,17 @@
+package com.example.game
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Menu_Coding : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu__coding)
+    }
+    fun  litt(view: View){
+        val  gameActivity: Intent = Intent(this@Menu_Coding,code_1::class.java)
+        startActivity(gameActivity)
+    }
+}
