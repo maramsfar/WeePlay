8c87d62ca046ebc996a94c8f2ffec855fe3a069a
CyrineM <82709871+CyrineM@users.noreply.github.com>
2021-05-04 00:20:30 +0100

    Update activityMain1.kt

--- activityMain1.kt
+++ activityMain1.kt
@@ -1,1 +1,17 @@
+package com.example.game
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun  lancerActivity2(view: View){
+        val  gameActivity: Intent = Intent(this@MainActivity,sign_in::class.java)
+        startActivity(gameActivity)
+    }
+}
