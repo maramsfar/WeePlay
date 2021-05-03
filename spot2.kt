294ecf810fedd71ab8db594c6728ca10ad8dfff1
CyrineM <82709871+CyrineM@users.noreply.github.com>
2021-05-04 00:43:20 +0100

    Update spot2.kt

--- spot2.kt
+++ spot2.kt
@@ -1,1 +1,52 @@
+package com.example.game
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+
+class Spot_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spot_2)
+
+    }
+    override fun onSupportNavigateUp():Boolean {
+        onBackPressed()
+        return true
+    }
+    fun rep1(view: View){
+        val inflater = layoutInflater
+        val dialogLayout: View = inflater.inflate(R.layout.activity_alert2, null)
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("False answer")
+        builder.setMessage("The correct answer is 8")
+        builder.setPositiveButton("Next"){dialogInterface, which ->
+            next(view)
+        }
+        builder.setView(dialogLayout)
+        builder.show()
+    }
+
+    fun next(view: View){
+        val gameActivity : Intent = Intent(this@Spot_2,spot_3::class.java)
+        startActivity(gameActivity)
+    }
+
+    fun rep2(view: View){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Correct !")
+        builder.setMessage("Continue to next level")
+        builder.setPositiveButton("Next"){dialogInterface, which ->
+            next(view)
+        }
+        builder.show()
+    }
+
+    fun back(view: View){
+        val gameActivity : Intent = Intent ( this@Spot_2,Menu_Spot::class.java)
+        startActivity(gameActivity)
+    }
+}
