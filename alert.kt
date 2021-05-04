e1f1b71b3852c63d4ce452a8c0fa56751ad4b3d1
CyrineM <82709871+CyrineM@users.noreply.github.com>
2021-05-04 01:02:31 +0100

    Update alert.kt

--- alert.kt
+++ alert.kt
@@ -1,1 +1,11 @@
+package com.example.game
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class alert_dialog_with_imageview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alert_dialog_with_imageview)
+    }
+}
