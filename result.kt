857fe1a4b6b881e9b2360c891834869423c51101
CyrineM <82709871+CyrineM@users.noreply.github.com>
2021-05-04 01:05:40 +0100

    Update result.kt

--- result.kt
+++ result.kt
@@ -1,1 +1,22 @@
+package com.example.game
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Result : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+    fun back(view: View){
+        val gameActivity : Intent = Intent ( this@Result,Menu_Spot::class.java)
+        startActivity(gameActivity)
+    }
+    fun cat(view: View){
+        val gameActivity : Intent = Intent ( this@Result,Game_Type::class.java)
+        startActivity(gameActivity)
+    }
+
+}
