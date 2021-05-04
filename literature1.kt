5046c7745a82ceb9e5ac30eb279ff1a5ce9eb57f
CyrineM <82709871+CyrineM@users.noreply.github.com>
2021-05-04 01:00:26 +0100

    Update literature1.kt

--- literature1.kt
+++ literature1.kt
@@ -1,1 +1,17 @@
+package com.example.game
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class litt_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_litt_1)
+    }
+    fun back(view: View){
+        val gameActivity : Intent = Intent ( this@litt_1,Menu_literature::class.java)
+        startActivity(gameActivity)
+    }
+}
